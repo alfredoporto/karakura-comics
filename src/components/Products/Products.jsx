0abf60979fb1011724ece15ae12999f846a9f919
Alfredo Portocarrero <benalfred42@gmail.com>
2022-02-07 10:50:24 -0500

    fix(Products): products and product components

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,9 +4,9 @@ import { Grid } from  '@material-ui/core';
 import Product from './Product/Product';
 
 const dummyProducts = [
-    { id: 1, name: 'Steel Ball Run 01', editorial:'Ivrea', price: 60 },
-    { id: 2, name: 'Steel Ball Run 02', editorial:'Ivrea', price: 60 },
-    { id: 3, name: 'Jujutsu Kaisen 13', editorial:'Norma', price: 40 }
+    { id: 1, name: 'Steel Ball Run 01', editorial:'Ivrea', price: 60, image: 'https://i1.whakoom.com/large/37/3f/ac9dad6b26a845b086b1e77d46b12631.jpg' },
+    { id: 2, name: 'Steel Ball Run 02', editorial:'Ivrea', price: 60, image: 'https://i1.whakoom.com/large/14/2a/fc474cfaea6742a48fb8dc43da44c26b.jpg' },
+    { id: 3, name: 'Jujutsu Kaisen 13', editorial:'Norma', price: 40, image: 'https://i1.whakoom.com/large/28/0b/d3db419132614a0b9491ee81304b42dd.jpg' }
 ];
 
 const Products = () => {
