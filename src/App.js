4b83c96daa8944f937091823839aef96e023371d
Alfredo Portocarrero <benalfred42@gmail.com>
2022-02-07 11:44:56 -0500

    feat: add navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+/*
 import Products from './components/Products/Products';
+import Navbar from './components/Navbar/Navbar';
+*/
+import { Products, Navbar } from './components';
 
 const App = () => {
   return (
-  <div>
-      Karakura Comics
-    <Products />
-  </div>);
+    <div>
+      <Navbar />
+      <Products />
+    </div>);
 };
 
 export default App;
