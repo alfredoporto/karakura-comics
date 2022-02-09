37f22a7edb523b521d8bc0eee2a82ce1e17f2fed
Alfredo Portocarrero <benalfred42@gmail.com>
2022-02-09 17:49:20 -0500

    feat: add cart component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import React from 'react';
 import Products from './components/Products/Products';
 import Navbar from './components/Navbar/Navbar';
 */
-import { Products, Navbar } from './components';
+import { Products, Navbar, Cart } from './components';
 
+{ /* ticket: implementar react router, llamar a la API de Commerce */ }
 const App = () => {
   return (
     <div>
       <Navbar />
       <Products />
+      <Cart />
     </div>);
 };
 
