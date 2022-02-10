dd6586c9a50239a4ea08fdb480f061e45ccbf923
Alfredo Portocarrero <benalfred42@gmail.com>
2022-02-10 16:08:53 -0500

    feat: add routing

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { Container, Typography, Button, Grid } from '@material-ui/core';
 import { mergeClasses } from '@material-ui/styles';
 
