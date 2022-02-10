97e1e85936ae7e0c648f283882e4bd17d5d25d7d
Alfredo Portocarrero <benalfred42@gmail.com>
2022-02-10 18:07:59 -0500

    build: add .env file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 /*
 import Products from './components/Products/Products';
 import Navbar from './components/Navbar/Navbar';
 */
 import { Products, Navbar, Cart } from './components';
+import { commerce } from './services/commerce';
 
 { /* ticket: implementar react router, llamar a la API de Commerce */ }
+
 const App = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+
+  const getProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  }
+  { /* Ni bien se cargue el componente en el DOM ejecutara getProducts */ }
+  useEffect(() => {
+    getProducts();
+    { /* Al pasarle un [] le digo a useEffect que solo quiero que lo ejecute al montar en el DOM, sino con cada update estaria pidiendo
+         los productos de la API */}
+    console.log(products);
+  }, []);
+
   return (
     <Router>
       <div>
