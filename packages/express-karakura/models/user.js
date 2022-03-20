98ae11e8560800aa9aab47837675ad45b36974f9
Alfredo Portocarrero <benalfred42@gmail.com>
2022-03-20 16:27:13 -0500

    refactor(User): add documentation

--- packages/express-karakura/models/user.js
+++ packages/express-karakura/models/user.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
-
+/*
+    Arreglo simple de passwords no recomendados, 
+    creo que la lista es de 1k palabras
+*/
 const weakPasswords = ["password", "123456"]
+/* 
+    "tokens" es un array de los tokens generados por
+    jwt; estos tokens seran usados para autenticacion
+
+    "timestamps" crea automaticamente los campos de
+    auditoria 'createdAt' y 'updatedAt para el model
+
+*/
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
