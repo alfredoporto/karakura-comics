4d7470db6ba05424c4fa066a2c73f574477c0d22
Alfredo Portocarrero <benalfred42@gmail.com>
2022-04-24 19:10:43 -0500

    refactor: routes

--- packages/express-karakura/app.js
+++ packages/express-karakura/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const userRouter = require('./routers/user')
 const bookRouter = require('./routers/book')
 const cartRouter = require('./routers/cart')
+const stripeRouter = require('./routers/stripe')
+const cors = require("cors")
 
 require('./db/mongoose')
 
@@ -9,10 +11,12 @@ const port = process.env.PORT
 
 const app = express()
 
-app.use(express.json())
-app.use(userRouter)
-app.use(bookRouter)
-app.use(cartRouter)
+app.use(cors());
+app.use(express.json());
+app.use("/api/users", userRouter);
+app.use("/api/books", bookRouter);
+app.use("/api/carts", cartRouter);
+app.use("/api/checkout", stripeRouter);
 
 app.listen(port, () => {
   console.log('server en: ' + port)
--- packages/express-karakura/db/mongoose.js
+++ packages/express-karakura/db/mongoose.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 console.log(process.env.MONGODB_URL)
-mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-});
+mongoose
+    .connect(process.env.MONGODB_URL,
+        {
+            useNewUrlParser: true,
+        })
+    .then(() => console.log("Mongo connection successfull"))
+    .catch((err) => {
+        console.log(err);
+    });
 
